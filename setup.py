b16d11bd57acad2619a795b2c365582d62080009
genwch <genwch@hotmail.com>
2021-02-01 06:42:18 +0000

    upd setup,py

--- setup.py
+++ setup.py
@@ -1,27 +1,29 @@
 import setuptools
-
+import os
+pkgname = os.path.basename(os.getcwd())
+with open(".CURVER", "r", encoding="utf-8") as fh:
+    curver = fh.readline()
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
+f = open("requirements.txt", "r")
+depends = f.read().split("\n")
+f.close()
 
 setuptools.setup(
-    name="gwws-genwch",  # Replace with your own username
-    version="0.0.6",
+    name=f"{pkgname}-genwch",  # Replace with your own username
+    version=f"{curver}",
     author="genwch",
     author_email="",
-    description="webscrape functions",
+    description=f"{pkgname}",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/genwch/gwws",
+    url=f"https://github.com/genwch/{pkgname}",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=[
-        "gwcomm-genwch",
-        "requests",
-        "BeautifulSoup4"
-    ],
+    install_requires=depends,
     python_requires='>=3.7',
 )
